05b09ae09cf3e79f7ddde805e4b8fc50d16a24b5
BrodieLawson <46755996+BrodieLawson@users.noreply.github.com>
2021-11-20 01:03:07 +1030

    Fix DrawText zone and giving cash + random chance of rubber
    Use whatever out of this you want, but note the Drawtext needs fixing regardless and the cash doesn't give correctly.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,9 @@
 fx_version 'cerulean'
 game 'gta5'
 
-shared_script 'config.lua'
+shared_scripts { 
+	'@qb-core/import.lua',
+	'config.lua'
+}
 client_script 'client/*.lua'
 server_script 'server/*.lua'
